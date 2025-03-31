960160de5ef8d720dd41a1d763d23bd12da31413
Yiğit Cankurtaran <yigitcankurtaranyc@gmail.com>
2025-03-31 16:24:14 +0300

    wrote first program

---
+++ chap1/firstprog.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using std::cin;
+using std::cout;
+using std::endl;
+
+int main()
+{
+    cout << "enter 2 numbers: " << endl;
+    int v1 = 0, v2 = 0;
+    cin >> v1 >> v2;
+
+    cout << "sum of " << v1 << " and " << v2 << " is " << v1 + v2 << endl;
+    return 0;
+}
