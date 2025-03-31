ef634832e38f8a39fe9e695750497acd8d199d60
Yiğit Cankurtaran <yigitcankurtaranyc@gmail.com>
2025-03-31 16:46:00 +0300

    solving the control flow exercise

---
+++ chap1/controlflowexercise.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+int main()
+{
+    // 1.9
+    int i = 50;
+    int sum = 0;
+    while (i <= 100)
+    {
+        sum += i;
+        i += 1;
+        std::cout << "i is currently " << i << std::endl;
+        std::cout << "sum is currently " << sum << std::endl;
+    }
+
+    // 1.10
+    int j = 10;
+    while (j > 0)
+    {
+        i--;
+        std::cout << "j is currently " << j << std::endl;
+    }
+
+    // 1.11 will be solved later
+}
